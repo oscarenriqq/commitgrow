b6dcc169a64f4194a877caf8b6c22d64124dd0c5
Oscar Molinar <molinar.o@latamdigitalmarketing.com>
2023-09-24 11:50:46 -0500

    default created_at, updated_at

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Table, Column
 from sqlalchemy.sql.sqltypes import Integer, String, DateTime, Boolean
 from config.db import get_metadata, get_engine
+import datetime
 
 users = Table(
     "users",
@@ -9,8 +10,8 @@ users = Table(
     Column("name", String(255), nullable=False),
     Column("email", String(255), nullable=False, unique=True),
     Column("password", String(255), nullable=False),
-    Column("created_at", DateTime, nullable=False),
-    Column("updated_at", DateTime, nullable=False),
+    Column("created_at", DateTime, nullable=False, default=datetime.datetime.now()),
+    Column("updated_at", DateTime, nullable=False, default=datetime.datetime.now()),
 )
 
 get_metadata().create_all(get_engine(), tables=[users])
