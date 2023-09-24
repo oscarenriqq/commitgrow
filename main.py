2810fe4f8fa11e00aa1c92579bc01fe551d39bec
Oscar Molinar <molinar.o@latamdigitalmarketing.com>
2023-09-23 20:20:14 -0500

    probando

--- main.py
+++ main.py
@@ -10,6 +10,3 @@ app.include_router(user, prefix="/api")
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
