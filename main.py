5ef97432866dc0b5cf97ed2a4e0d08bd0c6a6895
Oscar Molinar <molinar.o@latamdigitalmarketing.com>
2023-09-23 20:09:21 -0500

    deploying

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-#https://todoist.com/oauth/authorize?client_id=8ded4bfa03754acab19effa822a484c0&scope=data:read&state=secretstring
-
 from fastapi import FastAPI
 from routes.user import user
+import uvicorn
+import os
 
 app = FastAPI()
 
 app.include_router(user, prefix="/api")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
