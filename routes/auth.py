6cfd0ff604134344bc051b640d1d18118357055c
Oscar Molinar <molinar.o@latamdigitalmarketing.com>
2023-09-24 20:05:24 -0500

    prueba para obtener access_token

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,24 @@
 from fastapi import APIRouter
+from dotenv import load_dotenv
+import requests
+import os
 
 auth = APIRouter()
 
+load_dotenv()
+
 @auth.get("/redirect")
 def redirect(code: str, state: str):
-    return {"code": code, "state": state}
+    
+    response = requests.post(
+        "https://todoist.com/oauth/access_token", 
+        params= {
+            "client_id": os.getenv('TODOIST_CLIENT_ID'),
+            "client_secret": os.getenv('TODOIST_CLIENT_SECRET'),
+            "code": code
+        }
+    )
+    
+    data = response.json()
+    
+    return data
