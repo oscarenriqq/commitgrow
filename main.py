2c468308f7d8d85ede5de0e1be5c2eedefbbad07
Oscar Molinar <molinar.o@latamdigitalmarketing.com>
2023-10-18 22:10:50 -0500

    todoist authentication

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from routes.contract import contract_route
-from routes.todoist import task_router
+from routes.todoist import todoist_router
 from routes.auth import auth_router
 from routes.user import user_route
 
@@ -15,7 +15,7 @@ load_dotenv()
 app.include_router(user_route)
 app.include_router(auth_router)
 app.include_router(contract_route)
-app.include_router(task_router)
+app.include_router(todoist_router)
 
 @app.on_event("startup")
 async def startup():
--- routes/todoist.py
+++ routes/todoist.py
@@ -11,14 +11,25 @@ from schemas.user_auth import UserAuth
 from config.db import database
 from app.deps import get_current_user
 
-task_router = APIRouter()
+todoist_router = APIRouter()
 
 load_dotenv()
 
-@task_router.get("/redirect")
-def redirect(code: str, state: str, current_user: UserAuth=Depends(get_current_user)):
+@todoist_router.post("/authorize")
+async def authorize(secret_string: str, current_user: UserAuth = Depends(get_current_user)):
     
-    print(current_user)
+    query = users_todoist_credentials.select().where(users_todoist_credentials.c.secret_string == secret_string)
+    result = await database.execute(query)
+    
+    if result is None:
+        users_todoist_credentials_data = { "user_id": current_user.id, "secret_string": secret_string }
+            
+        query_insert = users_todoist_credentials.insert().values(users_todoist_credentials_data)
+        await database.execute(query_insert)
+    return JSONResponse(content={ "url": "{}{}".format(os.getenv('TODOIST_AUTH_URL'),secret_string) }, status_code=status.HTTP_200_OK)
+
+@todoist_router.get("/redirect")
+async def redirect(code: str, state: str):
     
     response = requests.post(
         url="https://todoist.com/oauth/access_token", 
@@ -29,22 +40,24 @@ def redirect(code: str, state: str, current_user: UserAuth=Depends(get_current_u
         }
     )
     
+    todoist_auth_data = response.json()
+    
+    query_user = users_todoist_credentials.select().where(users_todoist_credentials.c.secret_string == state)
+    user = await database.execute(query_user).fetchone()
+    
     user_todoist_data = {
-        "user_id": current_user.id,
-        "secret_string": state,
-        "access_token": data["access_token"],
+        "access_token": todoist_auth_data['access_token'],
     }
     
-    data = response.json()
-    query = users_todoist_credentials.insert().values(user_todoist_data)
-    result = database.execute(query)
+    query = users_todoist_credentials.update().where(users_todoist_credentials.c.user_id == user.id).values(user_todoist_data)
+    await database.execute(query)
 
-    return JSONResponse(content={ "message": "success" }, status_code=status.HTTP_200_OK)
+    return JSONResponse(content={ "message": "Todoist Authentication Complete." }, status_code=status.HTTP_200_OK)
 
-@task_router.get("/todoist-tasks")
+@todoist_router.get("/todoist-tasks")
 def get_tasks():
     return JSONResponse(status_code=200, content=todoist.get_todoist_tasks())
 
-@task_router.get("/todoist-task/{id}")
+@todoist_router.get("/todoist-task/{id}")
 def get_tasks(id: str):
     return JSONResponse(status_code=200, content=todoist.get_todoist_task(task_id=id))
