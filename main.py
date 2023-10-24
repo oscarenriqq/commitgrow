e85cba16949817796bc6b6ffaaf29a68b057b423
Oscar Molinar <molinar.o@latamdigitalmarketing.com>
2023-10-24 10:18:23 -0500

    agregando expose_headers parámetro a middleware

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],    
+    allow_headers=["*"],
+    expose_headers=["*"]    
 )
 
 load_dotenv() 
